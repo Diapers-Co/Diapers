1c783f4f06b355ceeb8bf5a045053795cdf68575
Ahmed-365 <61245411+Ahmed-365@users.noreply.github.com>
2020-04-15 18:58:32 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:baby_care/pages/settings.dart';
+import 'package:baby_care/pages/shop.dart';
+import 'package:baby_care/pages/profile.dart';
+
+void main() => runApp(new MyApp());
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+class MyAppState extends State<MyApp> {
+  int _selectedTab = 0;
+  final _pageOptions = [
+    Shop(),
+    Settings(),
+    Profile(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          primarySwatch: Colors.grey,
+          primaryTextTheme: TextTheme(
+            title: TextStyle(color: Colors.white),
+          )),
+      home: Scaffold(
+
+        body: _pageOptions[_selectedTab],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedTab,
+          selectedItemColor: Colors.lightBlueAccent,
+          onTap: (int index) {
+            setState(() {
+              _selectedTab = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              title: Text('Shop'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text('Profile'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }}
